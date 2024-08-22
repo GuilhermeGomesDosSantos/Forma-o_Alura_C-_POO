d59d3717d329668d92f391b1e6326ffcb1b55e22
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-08-22 10:08:05 -0300

    Variáveis do tipo inteiro

--- Formação_Alura_C#_POO/Program.cs
+++ Formação_Alura_C#_POO/Program.cs
@@ -24,6 +24,17 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção escolhida: "); // o comando write, é usado para o usuário digitar o número na mesma linha da mensagem e o WriteLine irá aparecer para digitar a informação na linha de baixo
+    string OpcaoEscolhida = Console.ReadLine()!;
+    /*Ele é usado para suprimir avisos de nulidade que o compilador pode gerar em situações onde ele não tem certeza se um valor pode ser null,
+     mas o programador sabe que o valor não será null em tempo de execução*/
+
+    int OpcaoEscolhidaNumerica = int.Parse(OpcaoEscolhida);
+    if (OpcaoEscolhidaNumerica == 1)
+    {
+        Console.WriteLine("Você escolheu a opção 1");
+    }
 }
 
 ExibirOpcoesDoMenu();
