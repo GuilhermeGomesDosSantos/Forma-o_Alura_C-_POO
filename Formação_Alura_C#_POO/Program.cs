d08a7f3d27b302973589a2c98f3ffb7cc64a50db
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-08-21 23:32:51 -0300

    Variáveis do tipo texto

--- Formação_Alura_C#_POO/Program.cs
+++ Formação_Alura_C#_POO/Program.cs
@@ -2,10 +2,28 @@
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
 void exibirMensagemDeBoasVindas()
-{ 
-    Console.WriteLine("****************************");
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
     Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("****************************");
 }
 
 exibirMensagemDeBoasVindas();
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+}
+
+ExibirOpcoesDoMenu();
