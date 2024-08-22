73bfa555e70f19846af6acca845d5ac6b19676c6
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-08-21 23:36:24 -0300

    Variáveis do tipo texto

--- Formação_Alura_C#_POO/Program.cs
+++ Formação_Alura_C#_POO/Program.cs
@@ -19,7 +19,7 @@ exibirMensagemDeBoasVindas();
 
 void ExibirOpcoesDoMenu()
 {
-    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
