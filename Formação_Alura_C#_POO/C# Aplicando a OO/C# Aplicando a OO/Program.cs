e73ff1bc629e76e65e0ba86fd2ebb6258cbd9d64
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-08-26 21:38:18 -0300

    Criando um método

--- Formação_Alura_C#_POO/C# Aplicando a OO/C# Aplicando a OO/Program.cs
+++ Formação_Alura_C#_POO/C# Aplicando a OO/C# Aplicando a OO/Program.cs
@@ -2,14 +2,18 @@
 
 musica1.nome = "Roxane";
 musica1.artista = "The police";
+musica1.duracao = 273;
+musica1.disponivel = true;
 
 Musica musica2 = new Musica();
 
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = false;
 
 /*Console.WriteLine($"Nome: {musica1}");
 como não foi adicionado nenhuma variavel da classe, será retornado, o proprio nome da classe*/
 
-Console.WriteLine($"Nome: {musica1.nome}");
-Console.WriteLine($"Artista: {musica2.artista}");
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
