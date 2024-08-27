4eb1cc565cb506b8eca85da9b6a6971fe509df32
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-08-26 21:25:29 -0300

    Objetos

--- Formação_Alura_C#_POO/C# Aplicando a OO/C# Aplicando a OO/Program.cs
+++ Formação_Alura_C#_POO/C# Aplicando a OO/C# Aplicando a OO/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();// essa variavel musica1, é do tipo musica
+
+musica1.nome = "Roxane";
+musica1.artista = "The police";
+
+Musica musica2 = new Musica();
+
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+
+/*Console.WriteLine($"Nome: {musica1}");
+como não foi adicionado nenhuma variavel da classe, será retornado, o proprio nome da classe*/
+
+Console.WriteLine($"Nome: {musica1.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");
