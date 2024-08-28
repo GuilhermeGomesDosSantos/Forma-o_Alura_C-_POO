e9018a1c451108d6ec22ebf1db744fa37f2fe5f5
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-08-27 22:19:35 -0300

    Atributos e propriedades

--- Formação_Alura_C#_POO/C# Aplicando a OO/C# Aplicando a OO/Program.cs
+++ Formação_Alura_C#_POO/C# Aplicando a OO/C# Aplicando a OO/Program.cs
@@ -1,16 +1,16 @@
 ﻿Musica musica1 = new Musica();// essa variavel musica1, é do tipo musica
 
-musica1.nome = "Roxane";
-musica1.artista = "The police";
-musica1.duracao = 273;
+musica1.Nome = "Roxane";
+musica1.Artista = "The police";
+musica1.Duracao = 273;
 musica1.Disponivel = true;
 Console.WriteLine(musica1.Disponivel);
 
 Musica musica2 = new Musica();
 
-musica2.nome = "Vertigo";
-musica2.artista = "U2";
-musica2.duracao = 367;
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
 musica2.Disponivel = false;
 
 /*Console.WriteLine($"Nome: {musica1}");
